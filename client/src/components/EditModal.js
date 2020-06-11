72fd465e8075b2e224c99315a37a6b24b4255d02
Hannah Davydova <hannah.davydova@gmail.com>
2020-06-11 15:03:51 +0100

    Add handleOnChange so input and textarea are editable

--- client/src/components/EditModal.js
+++ client/src/components/EditModal.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Modal from "@material-ui/core/Modal";
 import Input from "./Input";
 import Textarea from "./Textarea";
@@ -6,6 +6,22 @@ import Fab from "@material-ui/core/Fab";
 import CloseIcon from "@material-ui/icons/Close";
 
 function EditModal(props) {
+  const [note, setNote] = useState({
+    title: props.title,
+    content: props.content,
+  });
+
+  function handleOnChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevNote) => {
+      return {
+        ...prevNote,
+        [name]: value,
+      };
+    });
+  }
+
   return (
     <Modal
       open={props.openState}
@@ -14,8 +30,8 @@ function EditModal(props) {
       aria-describedby={"Note content:" + props.content}
     >
       <form className="create-note editModal">
-        <Input value={props.title} />
-        <Textarea value={props.content} rows={5} />
+        <Input onChange={handleOnChange} value={note.title} />
+        <Textarea onChange={handleOnChange} value={note.content} rows={5} />
         <Fab>
           <CloseIcon onClick={() => props.handleOnClose(props.id)} />
         </Fab>
